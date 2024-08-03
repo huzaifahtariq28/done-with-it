961d2566ebaa083db925dc967489c7003b2b82d5
Huzaifah Tariq <huzaifahtariq028@gmail.com>
2024-08-03 08:03:52 +0500

    added list item components and message screen

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessageScreen from './app/screens/MessageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <MessageScreen />
+    </GestureHandlerRootView>
+  );
 }
---
+++ app/screens/MessageScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+import ListItem from '../components/ListItem';
+import Screen from '../components/Screen';
+import ListItemSeparator from '../components/ListItemSeparator';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
+
+const initialMessages = [
+  {
+    id: 1,
+    title: 'T1',
+    description: 'D1',
+    image: require('../assets/huzaifah.jpg'),
+  },
+  {
+    id: 2,
+    title: 'T2',
+    description: 'D2',
+    image: require('../assets/huzaifah.jpg'),
+  },
+];
+
+function MessageScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log('List Item Selected', item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => setMessages(initialMessages)}
+      ></FlatList>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessageScreen;
